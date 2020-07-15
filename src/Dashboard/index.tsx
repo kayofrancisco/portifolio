bc5f679c25c773977d2c89c8a8dee272618d4629
kayofrancisco <kayofrancisco@hotmail.com>
2020-07-15 08:17:26 -0300

    Finalizado meu portifólio de códigos

---
+++ src/Dashboard/index.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
+import api from '../services/api';
+import { Repositories, Header, } from './styles';
+
+
+interface Repository {
+  name: string;
+  full_name: string;
+  description: string;
+  language: string;
+  html_url: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+   api.get<Repository[]>('/repos').then(({ data }) => {
+     setRepositories(data);
+   })
+  }, []);
+
+  return (
+    <>
+      <Header>
+        <img src="https://avatars3.githubusercontent.com/u/50703288?v=4" alt="Avatar do usuário" /> 
+        <div>
+          <div>
+            <strong>Kayo Francisco Silva Bezerra</strong>
+            <p>Desenvolvedor Java e Angular</p>
+          </div>
+          <section>
+            <a href="https://github.com/kayofrancisco">
+              <FaGithub size={36} title="Github" color="purple"/>
+            </a>
+            <a href="https://www.linkedin.com/in/kayo-francisco-51a816166/">
+              <FaLinkedin size={36} title="LinkedIn" color="blue"/>
+            </a>
+          </section>
+        </div>
+      </Header>
+      <Repositories>
+        {repositories.map(({full_name, name, description, language, html_url}) => (
+          <a key={full_name} href={html_url}>
+            <div>
+              <strong>{name}</strong>
+              <span>{language}</span>
+              <p>{description}</p>
+            </div>
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+}
+
+export default Dashboard;
---
+++ src/Dashboard/styles.ts
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  line-height: 56px;
+  max-width: 450px;
+  margin-top: 80px;
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a{
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.3s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+
+      span {
+        margin-left: 16px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
+
+export const Header = styled.div`
+  max-width: 100%;
+  display: flex;
+  align-items: stretch;
+
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+  }
+
+  div {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    div {
+      
+      strong {
+        margin-top: 20px;
+        font-size: 24px;
+        margin-left: 24px;
+      }
+
+      p {
+        font-size: 14px;
+        margin-left: 24px;
+      }
+    }
+
+    section {
+      a {
+        margin-left: 24px;
+      }
+    }
+  }
+`;
